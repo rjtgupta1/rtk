62f889efb3a17fd4eb7cc26a7c44d685fafe86f0
Rajat <imrjtgupta@gmail.com>
2023-08-10 13:39:49 +0530

    redux toolkit demo completed

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userSlice from "./slices/userSlice";
 import counterSlice from "./slices/counterSlice";
 
 const store = configureStore({
     reducer:{
-        users:userSlice.reducer,
         counter:counterSlice.reducer,
     }
 })
